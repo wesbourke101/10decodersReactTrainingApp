c6f0bb2211ab7e4b58c60fcf647e87995b15693e
Wes Bourke <98127652+wesbourke101@users.noreply.github.com>
2022-10-27 16:24:33 -0400

    added state rerenders and form now addeds new expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,7 +5,7 @@ import ExpensesFilter from "./ExpensesFilter";
 import './Expenses.css';
 
 function Expenses (props) {
-    const { expenses } = props
+    
     const [filteredYear, setFilteredYear] =useState('2020')
 
     const filterChangeHandler = selectedYear => {
@@ -16,26 +16,13 @@ function Expenses (props) {
         <div>
             <Card className="expenses">
                 <ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear}/>
-                <ExpenseItem
-                    title={expenses[0].title}
-                    amount={expenses[0].amount}
-                    date={expenses[0].date}
-                />
-                <ExpenseItem 
-                    title={expenses[1].title} 
-                    amount={expenses[1].amount} 
-                    date={expenses[1].date} 
-                />
-                <ExpenseItem 
-                    title={expenses[2].title} 
-                    amount={expenses[2].amount} 
-                    date={expenses[2].date} 
-                />
-                <ExpenseItem 
-                    title={expenses[3].title} 
-                    amount={expenses[3].amount} 
-                    date={expenses[3].date} 
-                />
+                {props.items.map((expense) => (
+                    <ExpenseItem 
+                        title={expense.title} 
+                        amount={expense.amount} 
+                        date={expense.date}
+                    />)
+                )}
             </Card>  
         </div>  
     );

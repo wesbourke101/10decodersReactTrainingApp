35ffa0c2db74d2a8e40eb4cbab4e64740ebd3001
Wes Bourke <98127652+wesbourke101@users.noreply.github.com>
2022-10-27 17:01:57 -0400

    added a filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,13 +11,17 @@ function Expenses (props) {
     const filterChangeHandler = selectedYear => {
         setFilteredYear(selectedYear)
     }
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear
+    })
 
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear}/>
-                {props.items.map((expense) => (
+                {filteredExpenses.map((expense, index) => (
                     <ExpenseItem 
+                        key={expense.id}
                         title={expense.title} 
                         amount={expense.amount} 
                         date={expense.date}
